71d5d275679c2e69e7dac9616eb80ff222b544fd
Pasan Dissanayake <pasand@boardpac.org>
2024-09-15 10:59:12 +0530

    Initial templtae

---
+++ TestApp/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Routers from "./Routers";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Routers />
+  </React.StrictMode>
+);
+
+
