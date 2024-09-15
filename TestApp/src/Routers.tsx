dae9355d268b9dc1dd6c05ce97af6e29baddc8a5
Pasan Dissanayake <pasand@boardpac.org>
2024-09-15 15:46:44 +0530

    Router modified

--- TestApp/src/Routers.tsx
+++ TestApp/src/Routers.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import App from "./App";
+import LoginPage from "./pages/LoginPage";
 
 function Routers() {
     return(
         <Routes>
             <Route path="/" element={<App />}></Route>
+            <Route path="/login" element={<LoginPage/>} />
         </Routes>
     );
 }
-
+    
 export default Routers;
